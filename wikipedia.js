beaf8ca2a4f0045a7f2f4811071df68e41b0e20a
Fabio Crisci <piuccio@gmail.com>
2020-04-12 19:13:50 +0900

    Remove html tags from english names

--- wikipedia.js
+++ wikipedia.js
@@ -4,12 +4,17 @@ const cached = require('@piuccio/cached-function');
 
 const WIKIPEDIA_COMPANY_CACHE = './input/cache/wikipedia-companies.json';
 const WIKIPEDIA_RAILWAY_CACHE = './input/cache/wikipedia-railways.json';
+const REDIRECTS = {
+  名古屋市: '名古屋市交通局',
+  福岡市: '福岡市交通局',
+  伊賀市: '伊賀鉄道',
+};
 
 function buildWikipediaUrl(title) {
   const searchParams = new URLSearchParams();
   searchParams.append('action', 'parse');
   searchParams.append('format', 'json');
-  searchParams.append('page', title);
+  searchParams.append('page', REDIRECTS[title] || title);
   searchParams.append('prop', 'wikitext|langlinks');
   searchParams.append('redirects', 1);
   searchParams.append('disabletoc', 1);
@@ -37,9 +42,9 @@ exports.getCompanyDetails = cached(WIKIPEDIA_COMPANY_CACHE, async function (comp
       } else {
         const [before, after = ''] = line.split('=');
         const key = before.substring(1).trim(); // substring removes the leading '|'
-        const value = after.trim();
-        if (after) {
-          details[MAPPING[key] || key] = after;
+        const [value] = after.trim().split('<');
+        if (value) {
+          details[MAPPING[key] || key] = value.trim();
         }
       }
     }
